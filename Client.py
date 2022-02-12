b2add5ba31be1c82a30e04da7226a0560b207d82
Ryan Eakin <eakinryan@hotmail.com>
2022-02-12 14:52:58 -0800

    Fixed message transmission

--- Client.py
+++ Client.py
@@ -8,9 +8,17 @@ import threading
 import time
 
 import logging
-logging.getLogger().handlers.clear()
-logging.basicConfig(filename="Clientout.csv",level=logging.DEBUG, format='%(message)s')
 
+from flask import request
+logging.getLogger().handlers.clear()
+
+#global variables
+workers = []
+thread_count = 1000
+bufferSize = 1025
+requestCount = 50
+
+logging.basicConfig(filename="Clientout"+str(thread_count)+".csv",level=logging.DEBUG, format='%(message)s')
 #----------------------------------------------------------------------------------------------------------------
 
 def clientThead(server_address, requestCount, message, thread_id):
@@ -32,30 +40,31 @@ def clientThead(server_address, requestCount, message, thread_id):
     for i in range(requestCount):
         start = time.time()
         total_bytes = sys.getsizeof(message)
-        sock.sendall(message) #sends the message to the server
+        sock.send(message) #sends the message to the server
         data = sock.recv(1024) #receives the message from the server
         end = time.time()
         total_delay = end - start
         total_requests += 1
+    sock.send(b'quit\n')
     duration_end = time.time()
     total_duration = duration_end - start_time
+    average_delay = total_delay / total_requests
 
     #recording results to log
-    logging.info("%d, %d, %d, %f, %f, %f" % (thread_id, total_requests, total_bytes, total_duration, total_delay, total_bytes/total_duration))
-    sock.close()
+    logging.info("%d, %d, %d, %f, %f, %f, %f" % (thread_id, total_requests, total_bytes, total_duration, total_delay, total_bytes/total_duration, average_delay))
+    # sock.close()
 
 def main(address, port):
     server_address = (address, port)
     print(server_address)
-    workers = []
-    thread_count = 100
-    message = "Hello World"
-    requestCount = 10
+    #fill message with random characters according to the buffersize
+    message = "a" * (bufferSize-2)+"\n"
     print("Starting %d threads" % thread_count)
     for i in range(thread_count):
         t = threading.Thread(target=clientThead, args=(server_address, requestCount, message, i))
         t.start()
         workers.append(t)
+
     for t in workers:
         t.join()
     
@@ -65,5 +74,5 @@ if __name__ == "__main__":
     if len(sys.argv) != 3:
         print('usage: %s <address> <port>' % sys.argv[0])
         sys.exit(1)
-    logging.info("Thread, Requests, Bytes, Duration, Delay, MB/s")
+    logging.info("Thread, Requests, Bytes, Duration, Delay, MB/s, Average Delay")
     main(sys.argv[1], int(sys.argv[2]))
