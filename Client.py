60226071505c81a4a1cc6bf6465b91e93d9c785c
Ryan Eakin <eakinryan@hotmail.com>
2022-02-12 12:59:57 -0800

    updated Client.py and added logging results to CSV file

--- Client.py
+++ Client.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+from concurrent.futures import thread
 import socket
 import select
 import sys
@@ -6,12 +7,18 @@ import os
 import threading
 import time
 
+import logging
+logging.getLogger().handlers.clear()
+logging.basicConfig(filename="Clientout.csv",level=logging.DEBUG, format='%(message)s')
+
 #----------------------------------------------------------------------------------------------------------------
 
-def clientThead(server_address, requestCount, message):
+def clientThead(server_address, requestCount, message, thread_id):
+    print("Starting client thread")
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.connect(server_address)
-
+    message = message.encode()
+    print("Connected to server")
     # record transfer information
     total_bytes = 0
     total_delay = 0
@@ -33,15 +40,20 @@ def clientThead(server_address, requestCount, message):
     duration_end = time.time()
     total_duration = duration_end - start_time
 
+    #recording results to log
+    logging.info("%d, %d, %d, %f, %f, %f" % (thread_id, total_requests, total_bytes, total_duration, total_delay, total_bytes/total_duration))
+    sock.close()
+
 def main(address, port):
     server_address = (address, port)
+    print(server_address)
     workers = []
     thread_count = 100
     message = "Hello World"
-    requestCount = 100
-
+    requestCount = 10
+    print("Starting %d threads" % thread_count)
     for i in range(thread_count):
-        t = threading.Thread(target=clientThead, args=(server_address, requestCount, message))
+        t = threading.Thread(target=clientThead, args=(server_address, requestCount, message, i))
         t.start()
         workers.append(t)
     for t in workers:
@@ -53,4 +65,5 @@ if __name__ == "__main__":
     if len(sys.argv) != 3:
         print('usage: %s <address> <port>' % sys.argv[0])
         sys.exit(1)
+    logging.info("Thread, Requests, Bytes, Duration, Delay, MB/s")
     main(sys.argv[1], int(sys.argv[2]))
